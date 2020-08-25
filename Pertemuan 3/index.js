6e608800ec9392c14b30ff217a64d17d1f12540a
Reinaldo Usadani <reinaldo.vanrulloff.96@gmail.com>
2020-08-25 14:31:30 +0800

    3rd Commit

---
+++ Pertemuan 3/index.js
@@ -0,0 +1,23 @@
+console.log("Hello World , from external file.")
+
+// Variabel dan Tipe Data
+// let digunakan pada variabel yang bisa diubah, const digunakan pada variabel yang tidak bisa diubah
+const firstName = "John"
+let age = 20
+age = 21
+const isMarried = true;
+
+console.log(typeof firstName)
+console.log(typeof age)
+console.log(typeof isMarried)
+const info = ("My Name is : " + firstName + ", I'm " + age + " years old. ")
+console.log(info)
+console.log(typeof info)
+
+// Operator
+
+let bilangan = 10;
+
+console.log(bilangan == "10") // loose equality
+console.log(bilangan === "10") // strict equality
+
