b22c78656b8f7faf1d11a12c425de923e0542dce
Reinaldo Usadani <reinaldo.vanrulloff.96@gmail.com>
2020-08-27 14:19:06 +0800

    4th Commit

---
+++ Pertemuan 4/index.js
@@ -0,0 +1,25 @@
+function penjumlahan(a,b){
+    let total
+    total = a+b
+
+    return total
+}
+
+console.log(penjumlahan(2,2))
+
+function penjumlahanDuaBujurSangkar(a,b){
+    
+    return a * a + b * b
+}
+
+const hasil = penjumlahanDuaBujurSangkar(8,3)
+console.log(hasil)
+
+// function tambah(a,b){
+//     return a + b
+// }
+// function kali(a,b){
+//     return a * b
+// }
+
+// console.log(tambah(kali(5,2), kali(10,2)))
